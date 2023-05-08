818dba091e3e7edd9218356f18485cf64d73e317
Saurav Meghwal <skbmasale941@gmail.com>
2023-05-09 03:50:26 +0530

    getting started

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  const successFunction = function (position) {
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+
+    var map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+
+    map.on('click', function (e) {
+      const { lat, lng } = e.latlng;
+      L.marker([lat, lng]).addTo(map).bindPopup('Jai Ho');
+    });
+  };
+
+  navigator.geolocation.getCurrentPosition(successFunction, e =>
+    console.error(e.message)
+  );
+}
