0ffce3736927e12635c2b46af536e09da57bd9ed
Saurav Meghwal <skbmasale941@gmail.com>
2023-05-10 06:53:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -99,15 +99,46 @@ class App {
   _newWorkout(e) {
     e.preventDefault();
 
-    // Clear input fields
-    inputCadence.value =
-      inputDistance.value =
-      inputDuration.value =
-      inputElevation.value =
-        '';
-
-    //Display marker
+    const validInputs = (...inputs) => inputs.every(e => Number.isFinite(e));
+    const allPositive = (...inputs) => inputs.every(e => e > 0);
     const { lat, lng } = this.#mapEvent.latlng;
+
+    // Get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+
+    // If activity running, create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+
+      // Check if data is valid
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs have to be positive numbers');
+    }
+
+    // If activity cycling, create cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+
+      // Check if data is valid
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      ) {
+        return alert('Inputs have to be positive numbers');
+      }
+    }
+
+    // Add new object to workotu array
+
+    // Render workout on map as marker
     L.marker([lat, lng])
       .addTo(this.#map)
       .bindPopup(
@@ -121,6 +152,17 @@ class App {
       )
       .setPopupContent(`workout`)
       .openPopup();
+
+    // Render workout on list
+
+    // Hide form
+
+    // Clear input fields
+    inputCadence.value =
+      inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
 
     form.classList.add('hidden');
   }
